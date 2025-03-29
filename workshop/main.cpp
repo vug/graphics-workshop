af0565533bf742cae6b5d5e968a43e0a4d169bd3
Ugur Guney <ugurguney@gmail.com>
2025-03-29 08:57:57 -0400

    bring GLFW + example code

--- workshop/main.cpp
+++ workshop/main.cpp
@@ -1,8 +1,56 @@
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
 #include <glad/gl.h>
 
 #include <print>
 
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
 int main() {
   std::println("Hi!");
+
+  if (!glfwInit()) {
+    std::println("Failed to initialize GLFW");
+    return -1;
+  }
+
+  glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+
+  const GLuint kWidth = 800, kHeight = 600;
+  GLFWwindow* window =
+      glfwCreateWindow(kWidth, kHeight, "LearnOpenGL", NULL, NULL);
+  if (!window) {
+    std::println("Failed to create GLFW window");
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+  glfwSetKeyCallback(window, keyCallback);
+
+  int version = gladLoadGL(glfwGetProcAddress);
+  if (version == 0) {
+    std::println("Failed to initialize OpenGL context via glad.");
+    return -1;
+  }
+  std::println("Loaded OpenGL version {}.{}", GLAD_VERSION_MAJOR(version),
+               GLAD_VERSION_MINOR(version));
+
+  glViewport(0, 0, kWidth, kHeight);
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+  }
+
   std::println("Bye!");
+  return 0;
+}
+
+void keyCallback(GLFWwindow* window, int key, [[maybe_unused]] int scancode, int action, [[maybe_unused]] int mode) {
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, GL_TRUE);
 }
