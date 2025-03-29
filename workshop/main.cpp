4ad582356d833cc54244c356ae59b9a244e005f1
Ugur Guney <ugurguney@gmail.com>
2025-03-29 18:10:02 -0400

    init ImGui usage

--- workshop/main.cpp
+++ workshop/main.cpp
@@ -1,6 +1,9 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include <glad/gl.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 #include <glm/glm.hpp>
 
 #include <print>
@@ -16,6 +19,7 @@ int main() {
   }
 
   glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+  glfwWindowHint(GLFW_SAMPLES, 8);
 
   const GLuint kWidth = 800, kHeight = 600;
   GLFWwindow* window =
@@ -37,18 +41,54 @@ int main() {
   std::println("Loaded OpenGL version {}.{}", GLAD_VERSION_MAJOR(version),
                GLAD_VERSION_MINOR(version));
 
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO& io = ImGui::GetIO();
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+  io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+  io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+  ImGui::StyleColorsDark();
+  // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+  ImGuiStyle& style = ImGui::GetStyle();
+  if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+    style.WindowRounding = 0.0f;
+    style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+  }
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  const char* kGlslVersion = "#version 460";
+  ImGui_ImplOpenGL3_Init(kGlslVersion);
+
   glm::vec3 origin{};
   std::println("Origin: ({}, {}, {})", origin.x, origin.y, origin.z);
 
   glViewport(0, 0, kWidth, kHeight);
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
 
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
+    ImGui::ShowDemoWindow();
+
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    ImGuiIO& io = ImGui::GetIO();
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+      GLFWwindow* backup_current_context = glfwGetCurrentContext();
+      ImGui::UpdatePlatformWindows();
+      ImGui::RenderPlatformWindowsDefault();
+      glfwMakeContextCurrent(backup_current_context);
+    }
     glfwSwapBuffers(window);
   }
+
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
+  glfwTerminate();
 
   std::println("Bye!");
   return 0;
